5a9d47c4ceff09acec04c419ca58540fbff971df
wangqing <wangqingi@dcits.com>
2017-02-07 16:01:33 +0800

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,23 @@
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
+	
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://'+arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 	def setUp(self):
 		self.browser=webdriver.Firefox()
@@ -19,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#王庆听说有个很好用的在线代办应用
 		#看了一下这个应用的首页
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		#他注意到网页的标题和头部都有"To-Do"这个词
 		self.assertIn('To-Do',self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -64,7 +79,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		
 		#天天访问首页
 		#页面中看不到王庆的清单
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacoke feathers',page_text)
 		self.assertNotIn('make a fly',page_text)
@@ -91,7 +106,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#访问首页
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024,768)
 
 		#输入框居中显示
