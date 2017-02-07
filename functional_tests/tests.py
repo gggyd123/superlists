fb4e1476ac71779703914d1318a7ddeda8925df3
wangqing <wangqingi@dcits.com>
2017-02-07 09:46:32 +0800

    first steps of FT for layout+styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -87,4 +87,22 @@ class NewVisitorTest(LiveServerTestCase):
 
 		#很满意，睡觉去了
 		
-		self.fail('Finish the test!')
+		#self.fail('Finish the test!')
+
+	def test_layout_and_styling(self):
+		#访问首页
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+
+		#输入框居中显示
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,512,delta=5)
+
+		#新建一个清单，输入框仍完美的居中显示
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		import time
+		time.sleep(3)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,512,delta=5)
+
